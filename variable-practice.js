42e8416050024b38b275b4a9eb108a2f7cd8fc83
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-09-10 08:32:39 +0800

    Add files via upload

---
+++ variable-practice.js
@@ -0,0 +1,11 @@
+let name = "Ronalyn Baniagas";
+console.log(name);
+
+let gender = "Female";
+console.log(gender);
+
+let whatDoYouWannaBecomeInYourLife = "programmer";
+console.log(whatDoYouWannaBecomeInYourLife);
+
+let twitterHandle = "@ronabelss13";
+console.log(twitterHandle);
